328bf10218a830f863bb248d2b8f1381970f28dc
ツAʙʜɪsʜᴇᴋ Kᴜᴍᴀʀ <93422036+MrAbhi2k3@users.noreply.github.com>
2023-03-04 00:26:40 +0530

    Update sample_info.py

--- sample_info.py
+++ sample_info.py
@@ -21,4 +21,3 @@ DATABASE_URI = "mongodb://[username:password@]host1[:port1][,...hostN[:portN]][/
 DATABASE_NAME = 'Telegram'
 COLLECTION_NAME = 'channel_files'  # If you are using the same database, then use different collection name for each bot
 
-
