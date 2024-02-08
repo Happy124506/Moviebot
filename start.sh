23aab72f36c19aa8f8d1446c5b73b73c03319f0e
Happy124506 <128205086+Happy124506@users.noreply.github.com>
2024-02-08 12:33:42 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/DX-Bots/SamanthaMoviesRobot.git /SamanthaMoviesRobot
+  git clone https://github.com/DX-Bots/Moviebot.git /Moviebot
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /SamanthaMoviesRobot
+  git clone $UPSTREAM_REPO /Moviebot
 fi
-cd /SamanthaMoviesRobot
+cd /Moviebot
 pip3 install -U -r requirements.txt
 echo "Starting SamanthaMoviesRobot...."
 python3 bot.py
